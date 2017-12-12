68799ab0ca21c30cac8933b0e89d5011082da178
aditya <aditya@UBMBP.local>
2017-12-12 17:32:04 +0530

    d

---
+++ atoz/atoz/main.cpp
@@ -0,0 +1,21 @@
+//
+//  main.cpp
+//  atoz
+//
+//  Created by aditya on 12/12/17.
+//  Copyright © 2017 aditya. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    char ch;
+    cout<<"the alphabets are:";
+    for(ch='a';ch<='z';ch++)
+    {
+        cout<<" "<<ch;
+    }
+    cout<<endl;
+    return 0;
+}
