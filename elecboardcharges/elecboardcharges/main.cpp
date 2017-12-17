57cca3d49443d257e45e611948940be9cfb102ee
aditya <aditya@UBMBP.local>
2017-12-17 16:05:22 +0530

    sdvgbfg

--- elecboardcharges/elecboardcharges/main.cpp
+++ elecboardcharges/elecboardcharges/main.cpp
@@ -13,13 +13,22 @@ int main()
     float units,charges;
     cout<<"enter the units:";
     cin>>units;
-    if(units ==100)
-        charges=0.04+50;
-   else if(units==200)
-        charges=0.05+50;
-     else   if(units==300)
-            charges=0.06+50;
-    else
+    if(units >=100)
+    {
+        charges=units*0.40;
+    charges=charges+50;
+    }
+     else if(units<=200||units>100)
+    {
+        charges=units*0.50;
+    charges=charges+50;
+    }
+      else if(units<=300||units>=200)
+      {
+            charges=units*0.60;
+    charges=charges+50;
+      }
+     else
         charges=0;
     cout<<"meter charge payed by user:"<<charges<<endl;
             }
