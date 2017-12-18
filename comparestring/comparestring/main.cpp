c671c45a5248199233fa89440cfa1b955355d20b
aditya <aditya@UBMBP.local>
2017-12-18 20:50:05 +0530

    bgf

--- comparestring/comparestring/main.cpp
+++ comparestring/comparestring/main.cpp
@@ -11,7 +11,7 @@ using namespace std;
 int main()
 {
     char name1[20],name2[20];
-    bool flag=1;
+    bool flag=0;
     cout<<"enter name1:";
     cin>>name1;// used cin instead of gets because it is unsafe
     cout<<"enter name2:";
@@ -25,7 +25,7 @@ int main()
         break;
     }
     if(flag==1)
-        cout<<"they are equal";
+        cout<<"they are equal"<<endl;
     else
         cout<<"they are not equal"<<endl;
     return 0;
---
+++ copystring/copystring/main.cpp
@@ -0,0 +1,21 @@
+//
+//  main.cpp
+//  copystring
+//
+//  Created by aditya on 12/18/17.
+//  Copyright © 2017 aditya. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    char name1[20],name2[20];
+    cout<<"enter name1:";
+    cin>>name1;
+    cout<<"enter name2:";
+    cin>>name2;
+    for(int i=0;name1[i]!='/0';i++)
+    {
+        
+}
