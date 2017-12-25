29e1e98b3897cfd67aafa4d096a921de519dfc36
aditya <aditya@UBMBP.local>
2017-12-25 15:55:26 +0530

    bv

---
+++ larnumarr/larnumarr/main.cpp
@@ -0,0 +1,29 @@
+//
+//  main.cpp
+//  larnumarr
+//
+//  Created by aditya on 12/25/17.
+//  Copyright © 2017 aditya. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int arr[50],size,i,large;
+    cout<<"enter the size of the array:";
+    cin>>size;
+    cout<<"enter the elements of the array";
+    for(i=0;i<size;i++)
+    {
+        cin>>arr[i];
+    }
+    large=arr[0];
+    for(i=1;i<size;i++)
+    {
+        if(large<arr[i])
+            large=arr[i];
+    }
+    cout<<"the largest element present is:"<<large<<endl;
+    return 0;
+}
