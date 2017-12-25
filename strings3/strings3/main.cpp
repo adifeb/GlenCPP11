81b13f7acc81421f9499c495fceb8cce52ed8e8c
aditya <aditya@UBMBP.local>
2017-12-25 20:49:35 +0530

    v

---
+++ strings3/strings3/main.cpp
@@ -0,0 +1,35 @@
+//
+//  main.cpp
+//  strings3
+//
+//  Created by aditya on 12/25/17.
+//  Copyright © 2017 aditya. All rights reserved.
+//
+
+#include <iostream>
+#include<stdio.h>
+#include<string.h>
+using namespace std;
+int main()
+{
+    char name[10],name1[10];
+    
+    cout<<"enter your first name:";
+    cin>>name;
+    cout<<"enter your last name:";
+    cin>>name1;
+    cout<<"the length of the first name is:"<<strlen(name)<<endl;
+    cout<<"the length of the last name is:"<<strlen(name1)<<endl;
+    strcat(name,name1);
+    cout<<"the concatination of the two variables are:"<<strcat(name,name1)<<endl;
+    char temp[10];
+    strcpy(temp,name);
+    strcpy(temp,name1);
+    cout<<"the copy string of the 2 names="<<strcpy(temp,name)<<strcpy(temp,name1)<<endl;
+    char com=strcmp(name,name1);
+    if (com==0)
+        cout<<com<<"the strings are equal"<<endl;
+    else
+        cout<<com<<"the strings  are not equal"<<endl;
+}
+
