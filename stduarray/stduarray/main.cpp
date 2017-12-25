226620b940e6285777a9e8a2bf46d2ea914d85c3
aditya <aditya@UBMBP.local>
2017-12-25 13:48:35 +0530

    f d

---
+++ stduarray/stduarray/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  stduarray
+//
+//  Created by aditya on 12/25/17.
+//  Copyright © 2017 aditya. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int marks[5]={19,10,5,6,7};
+    for(int i=0;i<5;i++)
+    {
+        cout<<"the marks of the student is:"<<marks[i]<<endl;
+    }
+}
