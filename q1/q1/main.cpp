5ae05c09f5b7278467cb28a91490b1ae8607edad
aditya <aditya@ubmbp.local>
2018-02-13 08:52:22 +0530

    vfcxds

---
+++ q1/q1/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  q1
+//
+//  Created by aditya on 2/13/18.
+//  Copyright © 2018 aditya. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num;
+    cout<<"enter the number:";
+    cin>>num;
+    cout<<"the number entered by you is:"<<num;
+}
---
+++ q2/q2/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  q2
+//
+//  Created by aditya on 2/13/18.
+//  Copyright © 2018 aditya. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    char ch;
+    cout<<"enter the character:";
+    cin>>ch;
+    cout<<"the ascii value is"<<int(ch);
+    return 0;
+}
