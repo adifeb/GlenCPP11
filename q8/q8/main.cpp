f5ba9181d863d47f8dfff902958ed7454ee32e37
aditya <aditya@ubmbp.local>
2018-02-13 11:59:45 +0530

    v

---
+++ q8/q8/main.cpp
@@ -0,0 +1,21 @@
+//
+//  main.cpp
+//  q8
+//
+//  Created by aditya on 2/13/18.
+//  Copyright © 2018 aditya. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n,i;
+    cout<<"enter the value:";
+    cin>>n;
+    cout<<"the multiplication table is:";
+    for(i=0;i<10;i++)
+    {
+        cout<<n*i;
+    }
+}
