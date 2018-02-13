58e0867425afa56e0ad89da81b2bbe7eae1ee41b
aditya <aditya@ubmbp.local>
2018-02-13 08:55:08 +0530

    vf

---
+++ q3/q3/main.cpp
@@ -0,0 +1,22 @@
+//
+//  main.cpp
+//  q3
+//
+//  Created by aditya on 2/13/18.
+//  Copyright © 2018 aditya. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num;
+    cout<<"enter the number:";
+    cin>>num;
+    if(num%2==0)
+        cout<<num<<"is even";
+    else
+        cout<<num<<"is odd";
+    return 0;
+    
+}
